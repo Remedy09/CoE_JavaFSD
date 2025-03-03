f689e08a6d7f8be756a9807f3e79ffe8db87ae77
unknown <manikandangb0903@gmail.com>
2025-03-03 13:55:20 +0530

    ''

---
+++ task5/e-commerce/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main style={{ padding: "20px" }}>Amazon Clone Content Here</main>
+    </div>
+  );
+}
+
+export default App;
